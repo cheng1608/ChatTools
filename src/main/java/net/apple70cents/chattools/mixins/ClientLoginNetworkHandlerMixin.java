b3bb3b7520b71609b0fcbd86f9ccabc354536b93
70CentsApple <Chris48@qq.com>
2024-07-14 18:01:59 +0800

    fix: add missing imports for older Minecraft versions

--- src/main/java/net/apple70cents/chattools/mixins/ClientLoginNetworkHandlerMixin.java
+++ src/main/java/net/apple70cents/chattools/mixins/ClientLoginNetworkHandlerMixin.java
@@ -11,6 +11,9 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+//#if MC<11900
+//$$ import net.minecraft.client.MinecraftClient;
+//#endif
 
 /**
  * @author 70CentsApple
