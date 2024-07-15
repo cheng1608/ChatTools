4d62cdd38b4bbb1a4548faa9af1a05b67a3f5c93
70CentsApple <Chris48@qq.com>
2024-07-15 20:34:27 +0800

    chore: remove duplicate import

--- src/main/java/net/apple70cents/chattools/mixins/ChatMessageC2SPacketMixin.java
+++ src/main/java/net/apple70cents/chattools/mixins/ChatMessageC2SPacketMixin.java
@@ -5,7 +5,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import net.apple70cents.chattools.ChatTools;
 import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(ChatMessageC2SPacket.class)
 public abstract class ChatMessageC2SPacketMixin {
