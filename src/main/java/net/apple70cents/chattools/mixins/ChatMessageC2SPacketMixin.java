68eb2431ee29f48185a0a14bbef0eb487ee3bb04
70CentsApple <Chris48@qq.com>
2024-07-15 20:33:09 +0800

    feat: increase chat character max length resolves #46

---
+++ src/main/java/net/apple70cents/chattools/mixins/ChatMessageC2SPacketMixin.java
@@ -0,0 +1,35 @@
+package net.apple70cents.chattools.mixins;
+
+import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import net.apple70cents.chattools.ChatTools;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ChatMessageC2SPacket.class)
+public abstract class ChatMessageC2SPacketMixin {
+    //#if MC>=11900
+    @ModifyConstant(method = "write", constant = @Constant(intValue = 256), require = 0)
+    private int increaseMaxLength(int endIndex) {
+        if (!(boolean) ChatTools.CONFIG.get("general.ChatTools.Enabled")) {
+            return 256;
+        }
+        if (!(boolean) ChatTools.CONFIG.get("general.IncreaseChatFieldMaxLength")) {
+            return 256;
+        }
+        return Integer.MAX_VALUE;
+    }
+    //#else
+    //$$ @ModifyConstant(method = "<init>(Ljava/lang/String;)V", constant = @Constant(intValue = 256), require = 0)
+    //$$ private int increaseMaxLength(int endIndex) {
+    //$$     if (!(boolean) ChatTools.CONFIG.get("general.ChatTools.Enabled")) {
+    //$$         return 256;
+    //$$     }
+    //$$     if (!(boolean) ChatTools.CONFIG.get("general.IncreaseChatFieldMaxLength")) {
+    //$$         return 256;
+    //$$     }
+    //$$     return Integer.MAX_VALUE;
+    //$$ }
+    //#endif
+}
--- src/main/java/net/apple70cents/chattools/mixins/ChatScreenMixin.java
+++ src/main/java/net/apple70cents/chattools/mixins/ChatScreenMixin.java
@@ -5,10 +5,12 @@ import net.apple70cents.chattools.features.general.ChatHistoryNavigator;
 import net.apple70cents.chattools.features.general.Translator;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.widget.TextFieldWidget;
+import org.apache.commons.lang3.StringUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ChatScreen.class)
@@ -16,7 +18,31 @@ public class ChatScreenMixin {
     @Shadow
     protected TextFieldWidget chatField;
 
-    @Inject(at = @At("HEAD"), method = "keyPressed")
+    @Inject(method = "init", at = @At("TAIL"))
+    private void increaseChatFieldMaxLength(CallbackInfo ci) {
+        if (!(boolean) ChatTools.CONFIG.get("general.ChatTools.Enabled")) {
+            return;
+        }
+        if (!(boolean) ChatTools.CONFIG.get("general.IncreaseChatFieldMaxLength")) {
+            return;
+        }
+        chatField.setMaxLength(Integer.MAX_VALUE);
+    }
+
+    //#if MC>=11900
+    @Inject(method = "normalize", at = @At("HEAD"), cancellable = true)
+    private void doNotNormalize(String text, CallbackInfoReturnable<String> cir) {
+        if (!(boolean) ChatTools.CONFIG.get("general.ChatTools.Enabled")) {
+            return;
+        }
+        if (!(boolean) ChatTools.CONFIG.get("general.IncreaseChatFieldMaxLength")) {
+            return;
+        }
+        cir.setReturnValue(StringUtils.normalizeSpace(text.trim()));
+    }
+    //#endif
+
+    @Inject(method = "keyPressed", at = @At("HEAD"))
     private void keyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
         if (!(boolean) ChatTools.CONFIG.get("general.ChatTools.Enabled")) {
             return;
