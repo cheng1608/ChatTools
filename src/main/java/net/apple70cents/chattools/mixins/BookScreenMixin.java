bb85bb98d53a4c15d6ed0db2e692e9f0f00d8dd5
70CentsApple <Chris48@qq.com>
2024-07-25 00:23:30 +0800

    chore: PreviewClickEvents also works for Book Screen

---
+++ src/main/java/net/apple70cents/chattools/mixins/BookScreenMixin.java
@@ -0,0 +1,25 @@
+package net.apple70cents.chattools.mixins;
+
+import net.apple70cents.chattools.ChatTools;
+import net.apple70cents.chattools.features.general.ClickEventsPreviewer;
+import net.minecraft.client.gui.screen.ingame.BookScreen;
+import net.minecraft.text.Style;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BookScreen.class)
+public abstract class BookScreenMixin {
+    @Inject(method = "getTextStyleAt", at = @At(value = "RETURN"), cancellable = true)
+    public void modifyHoverEvent(double x, double y, CallbackInfoReturnable<Style> cir) {
+        Style style = cir.getReturnValue();
+        if (!(boolean) ChatTools.CONFIG.get("general.ChatTools.Enabled")) {
+            cir.setReturnValue(style);
+        }
+        if (!(boolean) ChatTools.CONFIG.get("general.PreviewClickEvents.Enabled")) {
+            cir.setReturnValue(style);
+        }
+        cir.setReturnValue(ClickEventsPreviewer.work(style));
+    }
+}
