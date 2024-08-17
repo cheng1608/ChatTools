8d7a747e6b25a2f62c43c288edf93a8bdcce6e8d
70CentsApple <Chris48@qq.com>
2024-08-17 17:05:07 +0800

    perf: cache expensive nick hider operation (also removed some unused imports) resolves #52

--- src/main/java/net/apple70cents/chattools/features/general/NickHider.java
+++ src/main/java/net/apple70cents/chattools/features/general/NickHider.java
@@ -4,19 +4,33 @@ import net.apple70cents.chattools.ChatTools;
 import net.apple70cents.chattools.utils.TextUtils;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author 70CentsApple
  */
 public class NickHider {
+    private static final Map<String, Text> cache = new HashMap<>();
+
     public static Text work(Text message) {
         ClientPlayerEntity player = MinecraftClient.getInstance().player;
+        String nickname = TextUtils.encodeColorCodes((String) ChatTools.CONFIG.get("general.NickHider.Nickname"));
         if (player != null) {
-            if (TextUtils.wash(message.getString()).contains(player.getName().getString())) {
-                return TextUtils.replaceText((MutableText) message, player.getName().getString(),
-                        TextUtils.encodeColorCodes((String) ChatTools.CONFIG.get("general.NickHider.Nickname")));
+            String playerName = player.getName().getString();
+            String key = nickname + "|" + playerName + "|" + message.toString();
+
+            if (cache.containsKey(key)) {
+                return cache.get(key); // get from cache
+            }
+
+            // not cached, let's deal with it!
+            if (TextUtils.wash(message.getString()).contains(playerName)) {
+                Text result = TextUtils.replaceText(message.copy(), playerName, nickname);
+                cache.put(key, result); // put it in cache
+                return result;
             }
         }
         return message;
