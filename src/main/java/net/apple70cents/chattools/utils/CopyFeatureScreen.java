1411b1218efd0a5ca848ffe9303b0d7b83f03d89
70CentsApple <Chris48@qq.com>
2024-09-17 18:54:57 +0800

    feat: add copy screen preview

--- src/main/java/net/apple70cents/chattools/utils/CopyFeatureScreen.java
+++ src/main/java/net/apple70cents/chattools/utils/CopyFeatureScreen.java
@@ -1,15 +1,17 @@
 package net.apple70cents.chattools.utils;
 
-import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.MultilineText;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.MathHelper;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.HashMap;
+import java.util.Map;
 
 //#if MC>=12000
 import net.minecraft.client.gui.DrawContext;
@@ -21,68 +23,74 @@ public class CopyFeatureScreen extends Screen {
     private MultilineText messageSplit;
     private Screen oldScreen;
     private TextUtils.MessageUnit unit;
+    private Map<String, ButtonWidget> buttons;
+    private final Map<String, ButtonData> buttonDatas;
+    private MultilineText previewTextSplit;
+
+    protected static class ButtonData {
+        public int index;
+        public String content;
+
+        public ButtonData(int index, String content) {
+            this.index = index;
+            this.content = content;
+        }
+    }
 
     public CopyFeatureScreen(TextUtils.MessageUnit unit) {
         super(TextUtils.trans("texts.copy.title"));
         this.oldScreen = MinecraftClient.getInstance().currentScreen;
         this.messageSplit = MultilineText.EMPTY;
         this.unit = unit;
+        this.buttons = new HashMap<>();
+        this.buttonDatas = new HashMap<>();
     }
 
     @Override
     protected void init() {
         super.init();
         this.messageSplit = MultilineText.create(this.textRenderer, unit.message, this.width - 50);
+
+        this.buttonDatas.put("copyObjectData", new ButtonData(-3, unit.message.toString()));
+        this.buttonDatas.put("copyRaw", new ButtonData(-2, unit.message.getString()));
+        this.buttonDatas.put("copyWithColorCodeEscaped", new ButtonData(-1, TextUtils.decodeColorCodes(unit.message.getString())));
+        this.buttonDatas.put("copyWithNoColorCode", new ButtonData(0, TextUtils.wash(unit.message.getString())));
+        this.buttonDatas.put("copyUnixTimestamp", new ButtonData(1, String.valueOf(unit.unixTimestamp)));
+        this.buttonDatas.put("copyTimestamp", new ButtonData(2, this.getLongTimeDisplay(unit.unixTimestamp)));
+
         this.addButtons();
     }
 
     protected void addButtons() {
         int midH = this.height / 2;
-        Keyboard kb = MinecraftClient.getInstance().keyboard;
-        addCenterButton("copyObjectData", midH - 21 * 3, (button -> {
-            kb.setClipboard(unit.message.toString());
-        }));
-        addCenterButton("copyRaw", midH - 21 * 2, (button -> {
-            kb.setClipboard(unit.message.getString());
-        }));
-        addCenterButton("copyWithColorCodeEscaped", midH - 21, (button -> {
-            kb.setClipboard(TextUtils.decodeColorCodes(unit.message.getString()));
-        }));
-        addCenterButton("copyWithNoColorCode", midH, (button -> {
-            kb.setClipboard(TextUtils.wash(unit.message.getString()));
-        }));
-        addCenterButton("copyUnixTimestamp", midH + 21, (button -> {
-            kb.setClipboard(String.valueOf(unit.unixTimestamp));
-        }));
-        addCenterButton("copyTimestamp", midH + 21 * 2, (button -> {
-            Instant instant = Instant.ofEpochSecond(unit.unixTimestamp);
-            LocalDateTime currentTime = LocalDateTime.ofEpochSecond(unit.unixTimestamp, 0, ZoneId.systemDefault()
-                                                                                                 .getRules()
-                                                                                                 .getOffset(instant));
-            String offsetString = ZoneId.systemDefault().getRules().getOffset(instant).getId();
-            // yyyy/MM/dd HH:mm:ss UTC±XX:XX
-            String longTimeDisplay = String.format("%4d/%d/%d %d:%02d:%02d\nUTC%s", currentTime.getYear(), currentTime
-                    .getMonth()
-                    .getValue(), currentTime.getDayOfMonth(), currentTime.getHour(), currentTime.getMinute(), currentTime.getSecond(), offsetString);
-            kb.setClipboard(longTimeDisplay);
-        }));
+        for (Map.Entry<String, ButtonData> buttonData : buttonDatas.entrySet()) {
+            buttons.put(buttonData.getKey(), addCenterButton(buttonData.getKey(), midH + 21 * buttonData.getValue().index, (button -> {
+                MinecraftClient.getInstance().keyboard.setClipboard(buttonData.getValue().content);
+            })));
+        }
         addCenterButton("cancel", this.height - 30, (button) -> {
             MinecraftClient.getInstance().setScreen(oldScreen);
         });
     }
 
-    protected void addCenterButton(String translationKey, int y, ButtonWidget.PressAction func) {
+    protected ButtonWidget addCenterButton(String translationKey, int y, ButtonWidget.PressAction func) {
         int buttonW = 200;
         int buttonH = 20;
         //#if MC>=11900
-        this.addDrawableChild(ButtonWidget.builder(TextUtils.trans("texts.copy." + translationKey), func)
-                                     .position(this.width / 2 - buttonW / 2, y - buttonH / 2).size(buttonW, buttonH)
-                                     .build());
-        //#elseif MC>=11700
-        //$$ addDrawableChild(new ButtonWidget(this.width / 2 - buttonW / 2, y - buttonH / 2, buttonW, buttonH, TextUtils.trans("texts.copy." + translationKey), func));
+        ButtonWidget buttonWidget = ButtonWidget.builder(TextUtils.trans("texts.copy." + translationKey), func)
+                                                .position(this.width / 2 - buttonW / 2, y - buttonH / 2)
+                                                .size(buttonW, buttonH).build();
         //#else
-        //$$ addButton(new ButtonWidget(this.width / 2 - buttonW / 2, y - buttonH / 2, buttonW, buttonH, TextUtils.trans("texts.copy." + translationKey), func));
+        //$$ ButtonWidget buttonWidget = new ButtonWidget(this.width / 2 - buttonW / 2, y - buttonH / 2, buttonW, buttonH, TextUtils.trans("texts.copy." + translationKey), func);
         //#endif
+
+        //#if MC>=11700
+        this.addDrawableChild(buttonWidget);
+        //#else
+        //$$ addButton(buttonWidget);
+        //#endif
+
+        return buttonWidget;
     }
 
     @Override
@@ -98,6 +106,15 @@ public class CopyFeatureScreen extends Screen {
         // context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, this.getTitleY(), 16777215);
         // this draws the message
         this.messageSplit.drawCenterWithShadow(context, this.width / 2, this.getMessageY());
+        // this draws the content preview
+        Text previewText = TextUtils.SPACER;
+        for (Map.Entry<String, ButtonWidget> button : buttons.entrySet()) {
+            if (button.getValue().isMouseOver(mouseX, mouseY)) {
+                previewText = TextUtils.of(buttonDatas.get(button.getKey()).content);
+            }
+        }
+        this.previewTextSplit = MultilineText.create(this.textRenderer, previewText, this.width - 50);
+        this.previewTextSplit.drawCenterWithShadow(context, this.width / 2, this.height / 2 + 60);
     }
 
     private int getTitleY() {
@@ -111,6 +128,18 @@ public class CopyFeatureScreen extends Screen {
 
     private int getMessagesHeight() {
         return this.messageSplit.count() * 9;
+    }
+
+    private String getLongTimeDisplay(long timestamp) {
+        Instant instant = Instant.ofEpochSecond(timestamp);
+        LocalDateTime currentTime = LocalDateTime.ofEpochSecond(timestamp, 0, ZoneId.systemDefault().getRules()
+                                                                                    .getOffset(instant));
+        String offsetString = ZoneId.systemDefault().getRules().getOffset(instant).getId();
+        // yyyy/MM/dd HH:mm:ss UTC±XX:XX
+        String longTimeDisplay = String.format("%4d/%d/%d %d:%02d:%02d\nUTC%s", currentTime.getYear(), currentTime
+                .getMonth()
+                .getValue(), currentTime.getDayOfMonth(), currentTime.getHour(), currentTime.getMinute(), currentTime.getSecond(), offsetString);
+        return longTimeDisplay;
     }
 
 }
