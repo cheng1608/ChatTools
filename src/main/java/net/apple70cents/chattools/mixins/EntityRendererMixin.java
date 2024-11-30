f04459ad86f516aa7a86eade20dc5b54b7b415e6
70CentsApple <Chris48@qq.com>
2024-11-30 13:45:04 +0800

    build: bump to 1.21.3

--- src/main/java/net/apple70cents/chattools/mixins/EntityRendererMixin.java
+++ src/main/java/net/apple70cents/chattools/mixins/EntityRendererMixin.java
@@ -5,6 +5,7 @@ import net.apple70cents.chattools.features.bubble.BubbleRenderer;
 import net.apple70cents.chattools.features.general.NickHider;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.state.EntityRenderState;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.text.Text;
@@ -19,9 +20,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
  */
 @Mixin(EntityRenderer.class)
 public abstract class EntityRendererMixin {
+    //#if MC>=12002
+    Entity entity;
+    float tickDelta;
+    @Inject(method = "updateRenderState", at = @At(value = "HEAD"))
+    private void updateEntityAndTickDelta(Entity entity, EntityRenderState state, float tickDelta, CallbackInfo ci) {
+        this.entity = entity;
+        this.tickDelta = tickDelta;
+    }
 
     @Inject(method = "render", at = @At(value = "HEAD"))
-    private void render(Entity entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+    private void render(EntityRenderState state, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+        //TODO FIX chat bubbles
+    //#else
+    //$$ @Inject(method = "render", at = @At(value = "HEAD"))
+    //$$ private void render(Entity entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+    //#endif
         if (!((boolean) ChatTools.CONFIG.get("general.ChatTools.Enabled"))) {
             return;
         }
